204df96265da800dfe7fb49c3a4e215707a65d1b
Pithawat Nuckong <pithawat2624@gmail.com>
2023-01-14 13:26:20 +0700

    Add authorization middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,31 @@
+import dotenv from "dotenv";
+import { UnAuthenticatedError } from "../errors/index.js";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+dotenv.config();
+
 const auth = async (req, res, next) => {
-    console.log("Authenticate User here")
-    next()
-}
+	const userAuth = req.headers.authorization;
+
+	if (!userAuth || !userAuth.startsWith(process.env.SECRET_HEADER))
+		throw new UnAuthenticatedError("Authentication Invalid");
+
+	const token = userAuth.split(" ")[1];
+
+	try {
+		const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+
+		const user = await User.findById(userId);
+
+		if (!user) {
+			throw new UnAuthenticatedError("Authentication Invalid");
+		}
+
+		req.user = { ...user._doc };
+		next();
+	} catch (err) {
+		throw new UnAuthenticatedError("Authentication Invalid");
+	}
+};
 
 export default auth;
